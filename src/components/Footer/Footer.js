2590c311f00bc79486f098e8e5ac0dd9c472c704
Bojana K <36110963+bojana899@users.noreply.github.com>
2022-01-24 22:32:28 +0100

    Add files with upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by Bojana Kalkov
+    </div>
+  );
+};
+
+export default Footer;
